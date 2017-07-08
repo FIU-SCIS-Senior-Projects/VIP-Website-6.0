5344d2214444af0443f2465b4ce6f2b26738a7b7
Jose C Ponce <pepote5391@gmail.com>
2017-07-08 15:33:00 -0400

    Some changes to the ui part of the selenium tests

--- Tests/SeleniumTests/src/test/java/edu/fiu/vip_web/vip_r5_stories/common/ui/AdminPanelPage.java
+++ Tests/SeleniumTests/src/test/java/edu/fiu/vip_web/vip_r5_stories/common/ui/AdminPanelPage.java
@@ -11,17 +11,17 @@ public class AdminPanelPage {
     public static final By GENDER_FILTER_TEXTBOX = By.xpath("(//input[@type='text'])[8]");
     public static final By EMAIL_FILTER_TEXTBOX = By.xpath("(//input[@type='text'])[3]");
 
-    public static final String XPATH_FIRST_NAME_FIELD_FORMAT = "//table[1]/tbody/tr[%d]/td[1]";
+    public static final String XPATH_FIRST_NAME_FIELD_FORMAT = "//table[1]/tbody/tr[%d]/td[2]";
 
-    public static final String XPATH_PROJECT_APPLICATION_DATE_FIELD_FORMAT = "//tbody/tr[%d]/td[14]";
-    public static final String XPATH_USER_REGISTRATION_DATE_FIELD_FORMAT = "//tbody/tr[%d]/td[15]";
-    public static final String XPATH_FIRST_LOGIN_DATE_FIELD_FORMAT = "//tbody/tr[%d]/td[16]";
+    public static final String XPATH_PROJECT_APPLICATION_DATE_FIELD_FORMAT = "//tbody/tr[%d]/td[15]";
+    public static final String XPATH_USER_REGISTRATION_DATE_FIELD_FORMAT = "//tbody/tr[%d]/td[16]";
+    public static final String XPATH_FIRST_LOGIN_DATE_FIELD_FORMAT = "//tbody/tr[%d]/td[17]";
 
-    public static final By FIRST_LOGIN_DATE_FIELD_FIRST = By.xpath("//tbody/tr[1]/td[16]");
+    public static final By FIRST_LOGIN_DATE_FIELD_FIRST = By.xpath("//tbody/tr[1]/td[17]");
 
-    public static final By PROJECT_APPLICATION_DATE_LABEL = By.xpath("//td[14]");
-    public static final By USER_REGISTRATION_DATE_LABEL = By.xpath("//td[15]");
-    public static final By FIRST_LOGIN_DATE_LABEL = By.xpath("//td[16]");
+    public static final By PROJECT_APPLICATION_DATE_LABEL = By.xpath("//td[15]");
+    public static final By USER_REGISTRATION_DATE_LABEL = By.xpath("//td[16]");
+    public static final By FIRST_LOGIN_DATE_LABEL = By.xpath("//td[17]");
     
     // ADDED BY DAFNA 
     
--- Tests/SeleniumTests/src/test/java/edu/fiu/vip_web/vip_r5_stories/common/ui/ApplyForProjectPage.java
+++ Tests/SeleniumTests/src/test/java/edu/fiu/vip_web/vip_r5_stories/common/ui/ApplyForProjectPage.java
@@ -29,7 +29,7 @@ public class ApplyForProjectPage {
     public static final By DEPARTMENT_SELECT = By.xpath("//form/div/table/tbody/tr[9]/td/select");
     public static final By DEPARTMENT_SELECT_SECOND_OPTION = By.xpath("//form/div/table/tbody/tr[9]/td/select/option[2]");
 
-    public static final By EXPERIENCE_TEXTBOX = By.xpath("//form/div/table/tbody/tr[10]/td/div/input");
+    public static final By EXPERIENCE_TEXTBOX = By.xpath("//form/div/table/tbody/tr[10]/td/skills/div/textarea");
 
     public static final By SUBMIT_BUTTON = By.xpath("//form/div/div[1]/input");
 }

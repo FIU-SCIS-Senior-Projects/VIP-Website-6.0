9a98a7c30631118e4599a52a759d6c7bac2120b1
Jose C Ponce <pepote5391@gmail.com>
2017-07-15 18:41:06 -0400

    Minor changes to the certificate renewal script.

--- Code/certificateRenewal.bash
+++ Code/certificateRenewal.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+SERVERJS_LOCATION="/var/www/VIP-FINAL/Code"
+VIP_WEB_DOMAIN="vip-dev.cis.fiu.edu"#note this needs to be changed depending on the server on which this is run
+
 /var/www/certbot-auto renew | grep 'No renewals were attempted.'
 if [ $? -ne 0 ]; then
     cp "/etc/letsencrypt/live/${VIP_WEB_DOMAIN}/privkey.pem" "${SERVERJS_LOCATION}/"
